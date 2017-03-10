8eec6e1c69acaf44cfb340409ed228adb0720754
agrove25 <koeswantoandrew@gmail.com>
2017-03-09 20:56:01 -0800

    03/09/2017

--- HW8/dlskeleton/randmain.mk
+++ HW8/dlskeleton/randmain.mk
@@ -1,10 +1,10 @@
 randlibhw.so: randlibhw.c
-	$(CC) randlibhw.c -shared -fPIC -mrdrnd -o randlibhw.so
+	gcc randlibhw.c -shared -fPIC -mrdrnd -o randlibhw.so
 
 randlibsw.so: randlibsw.c
-	$(CC) randlibsw.c -shared -fPIC -o randlibsw.so
+	gcc randlibsw.c -shared -fPIC -o randlibsw.so
 
 randmain: randmain.c
-	$(CC) -g -c randcpuid.c
-	$(CC) -g -c randmain.c
-	$(CC) $(CFLAGS) randcpuid.o randmain.o -ldl -Wl,-rpath=$(PWD) -o randmain
+	gcc -g -c randcpuid.c
+	gcc -g -c randmain.c
+	gcc randcpuid.o randmain.o -ldl -Wl,-rpath,$(PWD) -o randmain
